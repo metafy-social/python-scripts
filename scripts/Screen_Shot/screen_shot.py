4d2dd3460f9fc1f54f99ee86627cdc65a2bd3427
Mohd Arsh Ali <arshergon@gmail.com>
2022-09-25 15:02:44 +0430

    Update screen_shot.py

--- scripts/Screen_Shot/screen_shot.py
+++ scripts/Screen_Shot/screen_shot.py
@@ -5,7 +5,7 @@ def screenshot_fun():
 	print("Screenshot program  runs on console\nchange the sleep time if you can the screenshot captured the terminal also")
 	sleepTimer = 2
 	print(f"You've {sleepTimer} to minimize the terminal window")
-	sleep(2)
+	sleep(sleepTimer)
 	print("Captured the screen now")
 	screenshot_img = pyautogui.screenshot()
 	screenshot_img.save("screen.png")
