a138004954d7cdaf8287842569889f8292166fe7
shreyas-9 <60311883+shreyas-9@users.noreply.github.com>
2022-10-06 20:34:43 +0530

    Delete Schedule_Whatsapp_message.py

--- scripts/Schedule_Whatsapp_message.py
+++
@@ -1,6 +0,0 @@
-# importing the module
-import pywhatkit
-
-# sending message to receiver
-# using pywhatkit
-pywhatkit.sendwhatmsg("+91xxxxxxxxxx","trying to schedule my msg",20, 30)   #as of now the time is 20:21 so the message will be sent exactly at 20:30
