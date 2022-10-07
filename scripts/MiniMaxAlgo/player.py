d4dbbd9ed59115a26aa044723e293c9a60dc460b
P Gautam <92343715+PGautam27@users.noreply.github.com>
2022-10-07 19:11:28 +0530

    Removed random ai

--- scripts/MiniMaxAlgo/player.py
+++ scripts/MiniMaxAlgo/player.py
@@ -27,15 +27,6 @@ class HumanPlayer(Player):
             except ValueError:
                 print('Invalid square. Try again.')
         return val
-
-
-class RandomComputerPlayer(Player):
-    def __init__(self, letter):
-        super().__init__(letter)
-
-    def get_move(self, game):
-        square = random.choice(game.available_moves())
-        return square
     
 class SmartComputerPlayer(Player):
     def __init__(self, letter):
