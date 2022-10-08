49809ca99d2eea81dca5965b7bbd617c472487e5
unknown <tronn232003@gmail.com>
2022-10-08 06:16:50 +0100

    refactored code and added instructions

--- scripts/Text_Game/Text_Game.py
+++ scripts/Text_Game/Text_Game.py
@@ -3,11 +3,12 @@ import time
 import sys
 
 def start():
-    print('Hello you r in the future and r fighting a robot')
+    # story generated
+    print('Hello you are in the future and are fighting a robot')
     time.sleep(3)
     print('you dont know what to do, thats why im here')
     time.sleep(3)
-    print('I am a Simulated Artificial Machine but u can call me S.A.M.')
+    print('I am a Simulated Artificial Machine but you can call me S.A.M.')
     smileyface()
 
 def smileyface():
@@ -25,32 +26,39 @@ def smileyface():
     print('|________________|')
 
 
-
+# First obstacle
 def level1():
-    a = ''
-    while a != 'run away' and a != 'shoot robot in head':
-        a = input('u can currently do two things which is either run away or shoot robot in head, what do u choose: ')
-        if a == 'run away':
+    question_one = ''
+    while question_one != 'run away' and question_one != 'shoot robot in head':
+        question_one = input('You can currently do two things which is either run away or shoot robot in head, what do you choose: ')
+        
+        #if chosen option and outcome of option
+        if question_one == 'run away':
             time.sleep(3)
             print('Unfortunately you have now been hunted down by an army of robots and have been executed')    
             time.sleep(3)
             print('too bad')
-            res = input('Do you wanna restart: ')
-            if res == 'yes':
+            
+
+            # restart for when user fails
+            restart = input('Do you wanna restart: ')
+            if restart == 'yes':
                 start()
                 level1()
                 path()
                 again = choice()
-                wtf(again)
+                random_select(again)
             else:
                 print('bye')
                 sys.exit()       
-        elif a == 'shoot robot in head':
+        
+        elif question_one == 'shoot robot in head':
             time.sleep(3)
             print('Congratulations you shot the robot in the head and are not dead!!')
             
 
 def path():
+    # story generated
     time.sleep(3)
     print('You are running away from the robots but...')
     time.sleep(3)
@@ -63,18 +71,23 @@ def path():
     print('choose wisely!!!!!!! ')
     print()
 
+# Second obstacle
 def choice():
-    d = ''
-    while d != '1' and d != '2':
+    question_two = ''
+    while question_two != '1' and question_two != '2':
         time.sleep(1)
-        d = input('So which one do you want to go on, path 1 or 2: ')
-    return d
+        question_two = input('So which one do you want to go on, path 1 or 2: ')
+    return question_two
 
-def wtf(rightpath):
-    t = random.randint(1,2)
-    if rightpath == str(t):
+
+# random path chosen for user to be right or wrong
+def random_select(rightpath):
+    select = random.randint(1,2)
+
+    if rightpath == str(select):
         time.sleep(3)
         print('good you went down the correct path and cheated death!!')  
+
     else:
         time.sleep(3)
         print('oh no you have been spotted by the robots!!!')
@@ -82,13 +95,24 @@ def wtf(rightpath):
         print('and you have been shot multiple time and are now dead.')
 
 
-playAgain = "yes"
-while playAgain == "yes" or playAgain == "y":
+
+
+# option to play again at completion of game
+play_again = "yes"
+
+while play_again == "yes" or play_again == "y":
     start()
     level1()
     path()
     again = choice()
-    wtf(again)
+    random_select(again)
     time.sleep(1)
-    playAgain = input("""Do you want to play again? 
+
+    play_again = input("""Do you want to play again? 
     Type yes or y to play again and no or n if you want to stop: """)
+
+
+stop_game = "no"
+while stop_game == "no" or stop_game == "n":
+        print('bye')
+        break
