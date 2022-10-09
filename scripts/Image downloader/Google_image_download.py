859fc5eda0edf15d3d5143ad21e09b9de0de510f
Hemant <87967889+Hemant2801@users.noreply.github.com>
2022-10-09 11:41:43 +0530

    Image downlaoder

---
+++ scripts/Image downloader/Google_image_download.py
@@ -0,0 +1,6 @@
+# pip install simple_image_download
+
+from simple_image_download import simple_image_download as simp
+response = simp.simple_image_download
+i = 'name'
+response().download(i, 'total images')
