b004459a213c3ef7e0f9d34bc804fe9dab54f891
Hemant <87967889+Hemant2801@users.noreply.github.com>
2022-10-09 11:42:44 +0530

    Image downloader

--- scripts/Image downloader/Google_image_download.py
+++ scripts/Image downloader/Google_image_download.py
@@ -2,5 +2,7 @@
 
 from simple_image_download import simple_image_download as simp
 response = simp.simple_image_download
-i = 'name'
+
+# This code can be run in a loop
+i = 'name' #Name of the item
 response().download(i, 'total images')
