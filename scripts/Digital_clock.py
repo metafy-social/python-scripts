0de464591a20be404d5c9cf0efce13cac7532dc7
Diwas Shrestha <114587196+diwas777777@users.noreply.github.com>
2022-10-09 20:26:52 +0545

    Cool looking clock made using tkinter

---
+++ scripts/Digital_clock.py
@@ -0,0 +1,24 @@
+from tkinter import Label, Tk 
+import time
+
+app_window = Tk() 
+app_window.title("Digital Clock") 
+app_window.geometry("420x150") 
+app_window.resizable(1,1)
+
+text_font= ("Boulder", 68, 'bold')
+background = "#f2e750"
+foreground= "#363529"
+border_width = 25
+
+label = Label(app_window, font=text_font, bg=background, fg=foreground, bd=border_width) 
+label.grid(row=0, column=1)
+
+def digital_clock(): 
+    # Put %H instead of %I to change it to 24 hour format
+   time_live = time.strftime("%I:%M:%S")
+   label.config(text=time_live) 
+   label.after(200, digital_clock)
+
+digital_clock()
+app_window.mainloop()
