6ad546711eccbb5da03ef920196bf00a4787d8e2
nabroleonx <nabroleonx@gmail.com>
2022-10-14 23:59:16 +0300

    Add small fix

--- scripts/Lyrics Scraper/LyricsScraper.py
+++ scripts/Lyrics Scraper/LyricsScraper.py
@@ -13,9 +13,10 @@ def get_lyrics(artist, song):
             'div', class_='col-xs-12 col-lg-8 text-center').find_all('div')[5].text
         print(lyrics)
     except AttributeError:
-        print("Please make sure you have entered the correct name!")
+        print("Please make sure you have entered the correct name(i.e. Don't add spaces between words)!")
 
 
-artist = input("Enter the name of the artist/band: ").strip().lower()
-song = input("Enter the name of the song: ").strip().lower()
+artist = input(
+    "Enter the name of the artist/band: ").strip().lower().replace(' ', '')
+song = input("Enter the name of the song: ").strip().lower().replace(' ', '')
 get_lyrics(artist, song)
