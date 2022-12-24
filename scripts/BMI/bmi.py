5c972513dcdb1674fe55436b702a609895d76c3c
dumbprogrammer <102885633+Lilafian@users.noreply.github.com>
2022-12-23 22:54:53 -0600

    Edit bmi.py capitalization

--- scripts/BMI/bmi.py
+++ scripts/BMI/bmi.py
@@ -1,16 +1,16 @@
-Height=float(input("Enter your height in centimeters: "))
-Weight=float(input("Enter your Weight in Kg: "))
+Height=float(input("Enter your height in cm: "))
+Weight=float(input("Enter your Weight in kg: "))
 Height = Height/100
 BMI=Weight/(Height*Height)
-print("your Body Mass Index is: ",BMI)
+print("Your Body Mass Index (BMI) is: ",BMI)
 if(BMI>0):
 	if(BMI<=16):
-		print("you are severely underweight")
+		print("You are severely underweight")
 	elif(BMI<=18.5):
-		print("you are underweight")
+		print("You are underweight")
 	elif(BMI<=25):
-		print("you are Healthy")
+		print("You are Healthy")
 	elif(BMI<=30):
-		print("you are overweight")
-	else: print("you are severely overweight")
+		print("You are overweight")
+	else: print("You are severely overweight")
 else:("enter valid details")
